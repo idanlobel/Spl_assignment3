ef44dc546a9926946aba5d0aa76247edc642ca6b
bayan kassem <bayanka@post.bgu.ac.il>
2022-01-01 15:45:03 +0200

    added to pre_procces in clienet controller

--- Server/src/main/java/bgu/spl/net/srv/client_controller.java
+++ Server/src/main/java/bgu/spl/net/srv/client_controller.java
@@ -17,12 +17,14 @@ public class client_controller<T extends Serializable> implements Connections<St
     private int connection_ID;
    private Map<String, Pair<Integer,String>> clients_map;//username and pair of <ID,password>
     private Map<Integer, ConnectionHandler<String>> connectionHandlerList;//username and his specefic connection handler
+    private List<String> logged_in_clients;//list of usernames that are logged in
 
     public client_controller()
     {
         connection_ID=1;
         this.clients_map=new HashMap<String, Pair<Integer,String>>();
         this.connectionHandlerList=new HashMap<Integer,ConnectionHandler<String>>();
+        logged_in_clients=new ArrayList<>();
     }
 
 
@@ -98,29 +100,35 @@ public class client_controller<T extends Serializable> implements Connections<St
                 //we have to know the size of every string , i did byte[] and not list because there is tostring method to byte[]
 
                 byte[] username_in_bytes=new byte[username_length];
+                int usernameindex=0;
                 byte[] password_in_bytes=new byte[password_length];
+                int passwordindex=0;
                 byte[] birthday_in_bytes=new byte[birthday_length];
+                int birthdayindex=0;
                 current_byte=1;
 
 
                 //now we loop again to save values to a mathcing array
                 while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
                 {
-                    username_in_bytes[current_byte]=msg_in_bytes[current_byte];
+                    username_in_bytes[usernameindex]=msg_in_bytes[current_byte];
+                    usernameindex++;
                     current_byte++;
                 }
                 current_byte++;
 
                 while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
                 {
-                    password_in_bytes[current_byte]=msg_in_bytes[current_byte];
+                    password_in_bytes[passwordindex]=msg_in_bytes[current_byte];
+                    passwordindex++;
                     current_byte++;
                 }
                 current_byte++;
 
                 while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
                 {
-                    birthday_in_bytes[current_byte]=msg_in_bytes[current_byte];
+                    birthday_in_bytes[birthdayindex]=msg_in_bytes[current_byte];
+                    birthdayindex++;
                     current_byte++;
                 }
 
@@ -142,10 +150,128 @@ public class client_controller<T extends Serializable> implements Connections<St
 
                     this.send(this.clients_map.get(username).fst,"another user with same name already exists");// we need to implement this shit somewhere
                 }
+            }
+            case ("00000010")://login
+            {// have no clue what is captcha
+                int current_byte=1;
+                int username_length=0;
+                int password_length=0;
+                while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
+                {
+                    current_byte++;
+                    username_length++;
+                }
+                current_byte++;
+                while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
+                {
+                    current_byte++;
+                    password_length++;
+
+                }
+                current_byte++;
+
+
+
+                byte[] username_in_bytes=new byte[username_length];
+                int usernameindex=0;
+                byte[] password_in_bytes=new byte[password_length];
+                int passwordindex=0;
+                current_byte=1;
+
+                //now we loop again to save values to a mathcing array
+                while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
+                {
+                    username_in_bytes[usernameindex]=msg_in_bytes[current_byte];
+                    usernameindex++;
+                    current_byte++;
+                }
+                current_byte++;
+
+                while(Byte.toString(msg_in_bytes[current_byte])!="00000000")// while we havent reached the delimeter 0
+                {
+                    password_in_bytes[passwordindex]=msg_in_bytes[current_byte];
+                    passwordindex++;
+                    current_byte++;
+                }
+                current_byte++;
+
+
+
+                String username = new String(username_in_bytes, StandardCharsets.UTF_8);
+                String password = new String(password_in_bytes, StandardCharsets.UTF_8);
+
+
+                Integer current_ch_ID = 0;
+                for (Integer key: this.connectionHandlerList.keySet()) {// get the id of current ch
+                    if(this.connectionHandlerList.get(key).equals(ch))
+                    {
+                        current_ch_ID=key;
+                        break;
+                    }
+
+                }
+                if(!this.logged_in_clients.contains(username))
+                {
+                    if(this.clients_map.containsKey(username))
+                    {
+                        this.logged_in_clients.add(username);
+                        this.send(current_ch_ID,username+" successfully logged in");
+                    }
+                    else
+                    {
+                        this.send(current_ch_ID,"Error!! "+username+" doesn't  exists");
+                    }
+
+
+                    this.connectionHandlerList.put(connection_ID,ch);
+                    connection_ID++;
+
+
+                }
+                else
+                {
+
+                    this.send(current_ch_ID,"Error!! "+username+" is already logged in");
                 }
 
             }
+            case ("00000011")://logout
+            {
+                Integer current_ch_ID = 0;
+                for (Integer key: this.connectionHandlerList.keySet()) {// get the id of current ch
+                    if(this.connectionHandlerList.get(key).equals(ch))
+                    {
+                        current_ch_ID=key;
+                        break;
+                    }
+
+                }
+                String username = "";
+                for(String user:this.clients_map.keySet())
+                {
+                    if(this.clients_map.get(user).fst==current_ch_ID)
+                    {
+                        username=user;
+                        break;
+                    }
+                }
+
+                if(this.logged_in_clients.contains(username))
+                {
+                    this.logged_in_clients.remove(username);
+                    this.send(current_ch_ID,username+" logged out successfully");
+
+                }
+                else
+                {
+                    this.send(current_ch_ID,"Error!! "+username+" is not logged in");
+                }
+
+            }
+
+
         }
+    }
 
 
 
